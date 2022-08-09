a734930837a4d7ae3d31ccf601119fc9b2b5446a
Pallavi <Palluteke1993@gmail.com>
2022-08-09 16:49:49 +0530

    remove unwanted route

--- app.py
+++ app.py
@@ -28,9 +28,7 @@ def hello_world():
     return render_template('index.html', allTodo = allTodo)
     # return 'Hello, World!!!'
 
-@app.route('/search/<int:sno>', method=['GET'])
-def search(sno):
-    todo = Todo.query.filter_by(sno=sno).first()
+
 
 @app.route('/show')
 def product():
